e9b83f2b57b8d57038756b9e984bad637f3bdffb
willian <willianserafim2017@gmail.com>
2024-07-19 14:46:07 -0300

    feat: implementação de testes unitários para a classe UsuarioController

--- src/main/java/com/generation/blogpessoal/model/Usuario.java
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -90,4 +90,14 @@ public class Usuario {
 	public void setPostagem(List<Postagem> postagem) {
 		this.postagem = postagem;
 	}
+
+	public Usuario(long id, String nome, String usuario, String senha, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	public Usuario() {	}
 }
